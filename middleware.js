6a93ed2f44fb13a50ada35f67317ca4dd972a9ca
Rohit Yadav <20rohityadav01@gmail.com>
2023-10-23 19:36:34 +0530

    complete first web

---
+++ middleware.js
@@ -0,0 +1,58 @@
+const ExpressError = require('./utils/ExpressError');
+const { campgroundSchema, reviewSchema } = require('./schemas');
+const Campground = require('./models/campground');
+const Review = require('./models/review');
+module.exports.isLoggedIn = (req, res, next) => {
+    if (!req.isAuthenticated()) {
+        req.session.returnTo = req.originalUrl;
+        req.flash('error', 'You Must Be Signed In First!');
+        return res.redirect('/login');
+    }
+    next();
+}
+module.exports.storeReturnTo = (req, res, next) => {
+    if (req.session.returnTo) {
+        res.locals.returnTo = req.session.returnTo;
+    }
+    next();
+}
+module.exports.validateCampground = (req, res, next) => {
+
+    // const result = campgroundSchema.validate(req.body);
+    const { error } = campgroundSchema.validate(req.body);
+    if (error) {
+        const msg = error.details.map(el => el.message).join(',')
+        throw new ExpressError(msg, 400);
+    } else {
+        next();
+    }
+}
+module.exports.isAuthor = async (req, res, next) => {
+    const { id } = req.params;
+    const campground = await Campground.findById(id);
+    if (!campground.author.equals(req.user._id)) {
+        req.flash('error', 'Yo Do Not Have Permission To Do That!')
+        return res.redirect(`/campgrounds/${id}`);
+    }
+    next();
+}
+module.exports.isReviewAuthor = async (req, res, next) => {
+    const { id, reviewId } = req.params;
+    const review = await Review.findById(reviewId);
+    if (!review.author.equals(req.user._id)) {
+        req.flash('error', 'Yo Do Not Have Permission To Do That!')
+        return res.redirect(`/campgrounds/${id}`);
+    }
+    next();
+}
+module.exports.validateReview = (req, res, next) => {
+
+    // const result = reviewSchema.validate(req.body);
+    const { error } = reviewSchema.validate(req.body);
+    if (error) {
+        const msg = error.details.map(el => el.message).join(',')
+        throw new ExpressError(msg, 400);
+    } else {
+        next();
+    }
+}
---
+++ seeds/index.js
@@ -0,0 +1,46 @@
+const mongoose = require('mongoose');
+const cities = require('./cities');
+const { places, descriptors } = require('./seedHelpers')
+const Campground = require('../models/campground')
+
+
+mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp');
+main().catch(err => console.log(err));
+
+async function main() {
+    await mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp');
+    console.log("MONGO CONNECTION OPEN");
+    // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
+};
+const sample = array => array[Math.floor(Math.random() * array.length)];
+const seedDB = async () => {
+    await Campground.deleteMany({});
+    for (let i = 0; i < 50; i++) {
+        const random1000 = Math.floor(Math.random() * 1000);
+        const price = Math.floor(Math.random() * 20) + 10;
+        const camp = new Campground({
+            author: '652d72f3eb62ba28548e2290',
+            location: `${cities[random1000].city},${cities[random1000].state}`,
+            title: `${sample(descriptors)} ${sample(places)}`,
+            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.Recusandae, maxime corporis ullam possimus qui provident, blanditiis quo repellat expedita rem eaque quae voluptatibus obcaecati temporibus dolore voluptatem quos alias iusto!',
+            price,
+            images: [
+                {
+                    url: 'https://res.cloudinary.com/da9qjcuq2/image/upload/v1697651844/YelpCamp/znrsd5et7akh8jfbpmzj.jpg',
+                    filename: 'YelpCamp/znrsd5et7akh8jfbpmzj'
+
+                },
+                {
+                    url: 'https://res.cloudinary.com/da9qjcuq2/image/upload/v1697651844/YelpCamp/ghuwqiu7fvydrem8pxxc.jpg',
+                    filename: 'YelpCamp/ghuwqiu7fvydrem8pxxc'
+
+                }
+
+            ]
+        })
+        await camp.save();
+    }
+}
+seedDB().then(() => {
+    mongoose.connection.close();
+});
